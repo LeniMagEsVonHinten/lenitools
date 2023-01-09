d6e094c29d6a21f62dc7a9cf4085dcb417f8df9b
Leni mag es von hinten <leni.mag.es.von.hinten@gmail.com>
2023-01-09 08:17:12 +0100

    :wrench: add simple setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='leni-tools',
+    version='1.0',
+    packages=['leni-tools'],
+    url='https://github.com/lenimagesvonhinten/leni-tools',
+    license='Apache 2.0 License',
+    author='Leni mag es von Hinten',
+    author_email='leni.mag.es.von.hinten@googlemail.com',
+    description=''
+)
